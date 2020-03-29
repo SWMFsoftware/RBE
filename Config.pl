4f4f6ccd3c27efbebaa9ea712c6291a965ea0202
Xiantong Wang <xtwang@umich.edu>
2020-03-28 20:21:32 -0400

    Changing Address for UM GitLab

--- Config.pl
+++ Config.pl
@@ -14,7 +14,7 @@ our $Code      = "RBE";
 
 my $config = "share/Scripts/Config.pl";
 # get util and share
-my $GITCLONE = "git clone"; my $GITDIR = "herot:/GIT/FRAMEWORK/";
+my $GITCLONE = "git clone"; my $GITDIR = "git\@gitlab.umich.edu:swmf_software/";
 if (-f $config or -f "../../$config"){
 }else{
     `$GITCLONE $GITDIR/share.git; $GITCLONE $GITDIR/util.git`;
