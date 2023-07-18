8a623e7902c986c510e58b0ca209d986404c70a9
Gabor Toth <gtoth@csrwks2022-0212.engin.umich.edu>
2023-07-18 16:35:00 -0400

    From Gitlab to Github

--- Config.pl
+++ Config.pl
@@ -14,7 +14,7 @@ our $Code      = "RBE";
 
 my $config = "share/Scripts/Config.pl";
 # get util and share
-my $GITCLONE = "git clone"; my $GITDIR = "git\@gitlab.umich.edu:swmf_software/";
+my $GITCLONE = "git clone"; my $GITDIR = "git\@github.com:SWMFsoftware/";
 if (-f $config or -f "../../$config"){
 }else{
     `$GITCLONE $GITDIR/share.git; $GITCLONE $GITDIR/util.git`;
